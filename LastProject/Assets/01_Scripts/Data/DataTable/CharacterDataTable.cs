8c67faf9a85dc3ee1e6921cdf75bc3b2442edec6
Jaemin <30969773+IUCyH@users.noreply.github.com>
2023-09-27 11:03:10 +0900

    오류 해결

--- LastProject/Assets/01_Scripts/Data/DataTable/CharacterDataTable.cs
+++ LastProject/Assets/01_Scripts/Data/DataTable/CharacterDataTable.cs
@@ -10,9 +10,9 @@ public static class CharacterDataTable
 
     static CharacterDataTable()
     {
-        characterTable.Add(new PlayerInfo("신영", null, null,null,null));
+        characterTable.Add(new PlayerInfo("신영", "망상가득 활기찬 모범반장", "성별 : 여","별자리 : 쌍둥이 자리","특이사항 : 주변이 온통 상상으로 가득참"));
         characterTable.Add(new PlayerInfo("지은", "망상가득 활기찬 모범반장", "성별 : 여","별자리 : 쌍둥이 자리","특이사항 : 주변이 온통 상상으로 가득참"));
-        characterTable.Add(new PlayerInfo("재환", null, null,null,null));
+        characterTable.Add(new PlayerInfo("재환", "망상가득 활기찬 모범반장", "성별 : 여","별자리 : 쌍둥이 자리","특이사항 : 주변이 온통 상상으로 가득참"));
     }
 
     public static PlayerInfo GetInfo(int characterID)
