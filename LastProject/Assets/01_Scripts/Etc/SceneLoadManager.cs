ab1df06d3e388072c2be9e491fba3df5a54d97b1
Jaemin <ghdckdwn8456@gmail.com>
2023-09-28 13:47:26 +0900

    SceneManger에서 씬 로드 체크 관련 코드 일부 추가 및 TODO 주석 추가

--- LastProject/Assets/01_Scripts/Etc/SceneLoadManager.cs
+++ LastProject/Assets/01_Scripts/Etc/SceneLoadManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public enum Scene
 {
+    None = -1,
     Title,
     Game
 }
@@ -12,23 +13,27 @@ public enum Scene
 public class SceneLoadManager : Singleton_DontDestroy<SceneLoadManager>
 {
     AsyncOperation asyncOperation;
-
+    Scene currentLoadingScene = Scene.None;
     
-
     public void Load(Scene scene)
     {
-        SceneManager.LoadSceneAsync((int)scene);    
+        asyncOperation = SceneManager.LoadSceneAsync((int)scene);
+        currentLoadingScene = scene;
     }
 
     void Update()
     {
-        if (!asyncOperation.isDone)
+        if (asyncOperation != null && currentLoadingScene != Scene.None)
         {
-
-        }
-        else
-        {
-            //GameManager
+            if (!asyncOperation.isDone)
+            {
+                //TODO : 로딩 바 업데이트 로직 작성 및 로딩 중 할 일 작성
+            }
+            else
+            {
+                //TODO : 로딩 바 100%로 채우는 로직 작성 및 로딩이 끝난 후 할 일 작성
+                currentLoadingScene = Scene.None;
+            }
         }
     }
 }
